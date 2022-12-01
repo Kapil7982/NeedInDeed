1c91f0d4833dfc1cedfa2f40ea514fe15152320f
Kapil7982 <rathor798299@gmail.com>
2022-12-01 06:31:40 +0530

    Done

--- NeedInDeed/src/main/java/com/NeedInDeed/NeedInDeed.java
+++ NeedInDeed/src/main/java/com/NeedInDeed/NeedInDeed.java
@@ -7,10 +7,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class Shop2HomeApplication {
+public class NeedInDeed {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Shop2HomeApplication.class, args);
+		SpringApplication.run(NeedInDeed.class, args);
 	}
 
 }
