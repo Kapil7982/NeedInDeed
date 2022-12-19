41faaca6979b40f306f5b52b000883f2215b7c21
Kapil <103938868+Kapil7982@users.noreply.github.com>
2022-12-19 12:20:09 +0530

    Update Shop2HomeApplicationTests.java

--- NeedInDeed/src/test/java/com/shop2home/Shop2HomeApplicationTests.java
+++ NeedInDeed/src/test/java/com/shop2home/Shop2HomeApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Shop2HomeApplicationTests {
+class NeedInDeedTests {
 
 	@Test
 	void contextLoads() {
